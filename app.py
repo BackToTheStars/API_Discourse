2d4bae4860bd2ec9ce01979dc734fcea7f91d866
BackToTheStars <nikgrigoryev@gmail.com>
2020-02-22 20:06:37 -0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,59 @@
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+games = [
+          {
+            'gameId': 0,                # одновременно идёт много игр
+            'gameName': '',             # имя игры будет меняться от хода к ходу
+            'turns': [                  # игроки делают последовательные ходы
+              {
+                'turnId': 0,            # номер хода в игре 
+                'player': '',           # игрок, который сделал этот ход
+                'dateTime': '',         # дата и время хода игрока
+                'openingComment': '',   # общий открывающий комментарий хода
+                'mainText': '',         # основной текст хода
+                'link': '',             # http-ссылка на источник текста
+                'linkDateTime': '',     # дата и время публикации оригинала в ссылке
+                'quotes': [],           # цитаты
+                'comments': [],         # комментарии к цитатам
+                'closingComment': '',   # общий закрывающий комментарий хода
+              }
+            ],
+          }
+        ]  
+
+
+@app.route('/')
+def home():
+  return "Hello World!"
+
+# GET /game/turn
+# GET /game/turn/quotes
+# GET /game/turn/comments
+ 
+# POST /game/turn<string:name>/text
+# POST /game/turn/quotes
+# POST /game/turn/comments
+
+# DELETE /game/turn
+# DELETE /game/turn/comment
+
+# POST /game/turn<string:name>/text
+@app.route('/game<string:game>/turn<string:turn>/text', methods=['POST'])
+def postGameTurnText(game, turn):
+  pass
+
+# POST /game<string:game>/turn<string:turn>/quotes
+@app.route('/game<string:game>/turn<string:turn>/quotes', methods=['POST'])
+def postGameTurnQuotes(game, turn):
+  pass
+
+# GET /game/turn<string:name>/quotes
+@app.route('/game<string:game>/turn<string:turn>/quotes')
+def getGameTurnQuotes(game, turn):
+  pass
+
+app.run(port=5000)
+
