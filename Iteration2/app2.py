2889b9ebf2bf8a9a74a7c9e75c34aaf7ec56db93
BackToTheStars <nikgrigoryev@gmail.com>
2020-02-26 23:36:41 -0800

    Implemented APIs for games: show games, create game, show game

--- Iteration2/app2.py
+++ Iteration2/app2.py
@@ -1,5 +1,5 @@
 
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 
@@ -15,22 +15,28 @@ class Game(Resource):
     for game in games:
       if game['gameName'] == name:
         return game                      
-  
+    return {'game': None}, 404
+
   def post(self, name):
+    data = request.get_json()
     game = {
-      'gameId': 1,
+      'gameId': data['gameId'],
       'gameName': name,
       'turns': [],
     }
     games.append(game)
-    return game
+    return game, 201
+
+class GameList(Resource):
+  def get(self):
+    return {'games': games}
 
 
 
 api.add_resource(Game, '/game/<string:name>')    # GET http://127.0.0.1:5000/game/Proton Mass
+api.add_resource(GameList, '/games')
 
 
-
-app.run(port=5000)
+app.run(port=5000, debug=True)
 
 
