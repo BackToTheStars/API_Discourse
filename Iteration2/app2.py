e0cf4f280d094076fb90902093675bfa736c71e1
BackToTheStars <nikgrigoryev@gmail.com>
2020-02-26 23:00:40 -0800

    Update app2.py

--- Iteration2/app2.py
+++ Iteration2/app2.py
@@ -6,11 +6,26 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+games = []
 
 
 class Game(Resource):
+  
   def get(self, name):
-    return {'game': name}
+    for game in games:
+      if game['gameName'] == name:
+        return game                      
+  
+  def post(self, name):
+    game = {
+      'gameId': 1,
+      'gameName': name,
+      'turns': [],
+    }
+    games.append(game)
+    return game
+
+
 
 api.add_resource(Game, '/game/<string:name>')    # GET http://127.0.0.1:5000/game/Proton Mass
 
