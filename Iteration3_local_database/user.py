4dfab1aa23609772e093291d2d2cf49832ee5ce6
BackToTheStars <nikgrigoryev@gmail.com>
2020-03-01 02:08:02 -0800

    Update

--- Iteration3_local_database/user.py
+++ Iteration3_local_database/user.py
@@ -2,7 +2,6 @@
 import sqlite3
 from flask_restful import Resource, reqparse
 
-
 class User(object):
 
   def __init__(self, _id, username, password):
@@ -46,7 +45,6 @@ class User(object):
 
     return user
 
-
 class UserRegister(Resource):
 
   parser = reqparse.RequestParser()
@@ -62,8 +60,10 @@ class UserRegister(Resource):
   )
 
   def post(self):
-
     data = UserRegister.parser.parse_args()
+
+    if User.find_by_username(data['username']):
+      return {"message": "User with this username already exists"}, 400
 
     connection = sqlite3.connect('data.db')
     cursor = connection.cursor()
