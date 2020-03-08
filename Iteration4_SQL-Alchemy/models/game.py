fdc9de9a146d706af931960bf09d9eb9a8fbc065
BackToTheStars <nikgrigoryev@gmail.com>
2020-03-07 20:05:33 -0800

    Update game.py

--- Iteration4_SQL-Alchemy/models/game.py
+++ Iteration4_SQL-Alchemy/models/game.py
@@ -2,6 +2,8 @@
 import sqlite3
 
 class GameModel:
+  TABLE_NAME = 'games'
+
   def __init__(self, id, name, turns):
     self.id = id
     self.name = name
@@ -25,7 +27,7 @@ class GameModel:
   def insert(self):
     connection = sqlite3.connect('data.db')
     cursor = connection.cursor()
-    query = "INSERT INTO {table} VALUES (?, ?, ?)".format(table=cls.TABLE_NAME)
+    query = "INSERT INTO {table} VALUES (?, ?, ?)".format(table=self.TABLE_NAME)
     cursor.execute(query, (self.id, self.name, self.turns))
     connection.commit()
     connection.close()
@@ -33,7 +35,7 @@ class GameModel:
   def update(self): # получает словарь игры, где есть имя и ходы
     connection = sqlite3.connect('data.db')
     cursor = connection.cursor()
-    query = "UPDATE {table} SET turns=? WHERE gameName=?".format(table=cls.TABLE_NAME)  # UPDATE одну строку
+    query = "UPDATE {table} SET turns=? WHERE gameName=?".format(table=self.TABLE_NAME)  # UPDATE одну строку
     cursor.execute(query, (self.turns, self.name))
     connection.commit()
     connection.close()
