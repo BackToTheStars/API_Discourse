be285874e14f87913fe970690c3f31ca204be88f
BackToTheStars <nikgrigoryev@gmail.com>
2020-03-10 23:09:23 -0700

    Update school.py

---
+++ Iteration4_SQL-Alchemy/resources/school.py
@@ -0,0 +1,13 @@
+
+from flask_restful import Resource
+from models.school import SchoolModel
+
+class School(Resource):
+  def get(self, schoolName):
+    pass
+
+  def post(self, schoolName):
+    pass
+
+  def delete(self, schoolName):
+    pass 
