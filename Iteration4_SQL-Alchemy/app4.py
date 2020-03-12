5152c987dd8fe1896d6777996c1765fc1581c250
BackToTheStars <nikgrigoryev@gmail.com>
2020-03-11 23:17:58 -0700

    Updates

--- Iteration4_SQL-Alchemy/app4.py
+++ Iteration4_SQL-Alchemy/app4.py
@@ -8,8 +8,10 @@ from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
 from security import authenticate, identity
-from resources.user import UserRegister         # импорт из папки resources
+from resources.user import UserRegister        
 from resources.game import Game, GameList
+from resources.school import School, SchoolList   # так добавляется Resource
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
@@ -17,16 +19,20 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'app'                          # should be long and secure
 api = Api(app)
 
+
 @app.before_first_request
 def create_tables():
   db.create_all()
 
+
 jwt = JWT(app, authenticate, identity)          # creates new endpoint /auth
 
 
-api.add_resource(Game, '/game/<string:name>')    # GET http://127.0.0.1:5000/game/Proton
-api.add_resource(GameList, '/games')
 api.add_resource(UserRegister, '/register')
+api.add_resource(Game,         '/game/<string:name>')    # GET http://127.0.0.1:5000/game/Proton
+api.add_resource(GameList,     '/games')
+api.add_resource(School,       '/school/<string:schoolName>')  # так добавляется Resource
+api.add_resource(SchoolList,   '/schools')
 
 
 if __name__ == '__main__':        # запускаем только если этот файл запущен как главный
